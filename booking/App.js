ec8e4bec17142d913db22d3f89f66487cfa2a323
Yassin-hagenimana <hyassin509@gmail.com>
2022-05-17 12:55:11 +0200

    wecome

--- booking/App.js
+++ booking/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import {
+    StyleSheet,
+    Text,
+    Image,
+    SafeAreaView,
+    Button,
+    Alert,
+    Platform,
+    StatusBar,
+    Dimensions
+} from 'react-native';
+import {useDeviceOrientation} from "@react-native-community/hooks";
+import Welcome from "./app/Screens/Welcome";
 
 export default function App() {
+ const{landscape}=useDeviceOrientation();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        {/*<View style={{*/}
+        {/*    backgroundColor: 'dodgerblue',*/}
+        {/*    width:"50%",*/}
+        {/*    height:landscape? "100%":"30%"*/}
+        {/*}}>*/}
+
+        {/*</View>*/}
+      <Text>React Native</Text>
+      <Text numberOfLines ={10}>Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum </Text>
+
+        <Image source={{
+            width:300,
+            height:300,
+            uri:"https://picsum.photos/seed/picsum/200/300"
+        }}/>
+
+        <Text>Alerting button</Text>
+        <Button title="Click me" onPress={()=>Alert.alert("Hello Yassin")}/>
+
+           <Welcome/>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#fff',
+    justifyContent:"center",
+    alignItems:'center',
+      paddingTop:Platform.OS === "android" ? StatusBar.currentHeight:0
   },
 });
