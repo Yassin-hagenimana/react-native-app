dfebd117fc295f8a30cf2df578c8671697ea7b0f
Yassin-hagenimana <hyassin509@gmail.com>
2022-05-17 09:19:16 -0700

    login

---
+++ booking/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+function HomeScreen() {
+    return (
+       <View>
+
+
+       </View>
+    );
+}
+
+export default HomeScreen;
+
+const styles=StyleSheet.create({
+    
+})
